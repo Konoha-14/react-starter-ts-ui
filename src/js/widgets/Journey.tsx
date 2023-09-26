5499e72441e83529f433ac4b102de6a371b264d1
rvinian <richa_vinian@intuit.com>
2023-09-27 00:45:37 +0530

    feat: init project with vite, ts and linter

---
+++ src/js/widgets/Journey.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <div> Hello there traveller! How is your journey going so far... </div>
+  </React.StrictMode>
+)
